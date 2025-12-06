0ea8e08d0d5fa6577a14fa84010d0fa91fb8a008
Dmoth00 <115425307+Dmoth00@users.noreply.github.com>
2025-12-05 22:09:36 -0300

    Radio!
    Huh? Radio? What's going on with that radio?

---
+++ Deserter/Core/Props/radio.gd
@@ -0,0 +1,21 @@
+extends StaticBody3D
+@onready var light=$light
+@onready var smoke=$smoke
+@onready var col=$col
+var t = 0.0
+
+
+func _process(delta: float) -> void:
+	if light!=null:
+		t+=delta
+		if t>0.5: t=0
+		light.scale=Vector3.ONE*t
+	pass
+
+func _hurt(dmg : float):
+	if dmg>0.0:
+		light.queue_free()
+		smoke.emitting=true
+		col.disabled=true
+		set_deferred("process_mode","PROCESS_MODE_DISABLED")
+		pass
