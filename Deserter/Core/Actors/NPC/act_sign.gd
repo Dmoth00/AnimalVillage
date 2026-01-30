cbd6accf075a4c6c823d83540b5dccc897a807ec
ExtendComunicaciones <rmartinez@extend.cl>
2026-01-30 12:16:28 -0300

    furnishing continues

--- Deserter/Core/Actors/NPC/act_sign.gd
+++ Deserter/Core/Actors/NPC/act_sign.gd
@@ -1,7 +1,6 @@
 extends Area3D
-@export var texts : Array[String]
+@export var texts=""
 
-
-func act(player : CharacterBody3D) -> void:
+func act(_player : CharacterBody3D) -> void:
 	get_tree().get_first_node_in_group("TXT").act(texts)
 	pass
