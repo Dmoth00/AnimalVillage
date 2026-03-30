3993a41ae25d79d9c2d67d1af4969cf51522038e
Dmoth00 <115425307+Dmoth00@users.noreply.github.com>
2026-03-30 09:54:20 -0300

    watr shadr

---
+++ Deserter/Core/Actors/Player/PlayerScripts/step_ray.gd
@@ -0,0 +1,20 @@
+extends RayCast3D
+
+@onready var sfx=$ripples
+@onready var player=get_parent()
+var speed = 0.0
+
+func _on_check() -> void:
+	player.in_water=false
+	sfx.emitting=false
+	if is_colliding():
+		var other = get_collider()
+		if other.is_in_group("Water"):
+			player.in_water=true
+			speed=player.velocity.length()
+			sfx.global_position.y=get_collision_point().y+0.01
+			if speed>1.0:
+				sfx.emitting=true
+
+
+	pass # Replace with function body.
