a29d3e9e59361d0690e60d8a2c9d6ee12e64df0b
ExtendComunicaciones <rmartinez@extend.cl>
2026-05-12 13:41:16 -0400

    One way doors

---
+++ Deserter/Core/Props/iteractibles/one_way_door.gd
@@ -0,0 +1,39 @@
+extends Area3D
+
+@export var opensound : AudioStream
+@export var lockedSound : AudioStream
+var closedDialog : Array [String] = ["It doesn't open from this side."]
+var closed=true
+
+var id : String
+
+@onready var col=$doorcol
+@onready var anim=$anim
+
+func act(player : CharacterBody3D):
+	var p=player
+	if closed:
+		anim.play("anim_rattle")
+		doorSound(lockedSound,p)
+		get_tree().get_first_node_in_group("TXT").act(closedDialog)
+	else:
+
+		#all this is just bells and whistles
+		anim.play("anim_door")
+		doorSound(opensound,p)
+		
+		#the door opens
+		gvars.event_list.append(id)
+		col.queue_free()
+		monitorable=false
+
+
+func _on_opener_body_entered(_body: Node3D): closed=false
+
+func _on_opener_body_exited(_body: Node3D): closed=true # Replace with function body.
+
+func doorSound(sound : AudioStream, player : CharacterBody3D):
+	if sound:
+		var snd=player.get_node("SFX")
+		snd.stream = sound
+		snd.play()
