c775c4bf16552d19f8f8d18c3525980d12168943
ExtendComunicaciones <rmartinez@extend.cl>
2026-05-12 19:55:53 -0400

    fix one way doors

--- Deserter/Core/Props/iteractibles/one_way_door.gd
+++ Deserter/Core/Props/iteractibles/one_way_door.gd
@@ -4,11 +4,13 @@ extends Area3D
 @export var lockedSound : AudioStream
 var closedDialog : Array [String] = ["It doesn't open from this side."]
 var closed=true
+var over=false
 
 var id : String
 
 @onready var col=$doorcol
 @onready var anim=$anim
+@onready var col2=$col2
 
 func act(player : CharacterBody3D):
 	var p=player
@@ -17,15 +19,21 @@ func act(player : CharacterBody3D):
 		doorSound(lockedSound,p)
 		get_tree().get_first_node_in_group("TXT").act(closedDialog)
 	else:
-
+		#don't repeat
+		if over: return
+		over=true
+		
 		#all this is just bells and whistles
 		anim.play("anim_door")
 		doorSound(opensound,p)
 		
 		#the door opens
+		#the event trigger is saved
 		gvars.event_list.append(id)
+		#the collision is destroyed
 		col.queue_free()
-		monitorable=false
+		#the colider's shape is disabled.
+		col2.set_deferred("disabled",true)
 
 
 func _on_opener_body_entered(_body: Node3D): closed=false
